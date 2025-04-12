57863071e9eb274f8711cc3940ffc74e0dc092cb
zelick <kristinazelic0908@gmail.com>
2025-04-12 21:41:05 +0200

    Added PubSubEngine

---
+++ PubSubConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace PubSubConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var engine = new PubSubEngine();
+
+            // Pretplate korisnika
+            engine.Subscribe(1, msg => Console.WriteLine($"[Ana] Poruka: {msg}"));
+            engine.Subscribe(1, msg => Console.WriteLine($"[Kristina] Poruka: {msg}"));
+            engine.Subscribe(2, msg => Console.WriteLine($"[Milica] Poruka: {msg}"));
+
+            // Objavi poruku na temu 1 (Fotografije)
+            Console.WriteLine("Objavljujem novu poruku na temu 1 (Fotografije)...");
+            engine.Publish(1, "Nova pesma od izvođača X!");
+
+            // Objavi poruku na temu 2 (Sport)
+            Console.WriteLine("Objavljujem novu poruku na temu 2 (Sport)...");
+            engine.Publish(2, "Fudbalski tim Y je pobedio!");
+
+            // Objavi poruku na temu 3 (niko nije pretplaćen, neće se ništa desiti)
+            Console.WriteLine("Objavljujem na temu 3 (nepostojeća pretplata)...");
+            engine.Publish(3, "Ovo niko neće videti");
+
+            // Sačekaj malo da se niti završe
+            Thread.Sleep(1000);
+        }
+    }
+}
---
+++ PubSubConsoleApp/PubSubEngine.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PubSubConsoleApp
+{
+    public class PubSubEngine
+    {
+        // na primer int - topic id( muzika, sport, fotografija) 
+        // ana se pretplati na fotografije i muziku 
+        // kristina se pretplati na sport 
+        // milica se pretplati na sve 
+        //neko objavi poruku na temu Muzika, svi koji su pretplaceni dobijaju tu poruku 
+
+        //List<Action<string>> -  lista korisnika, odnosno funckija koja reaguje kada dodje poruka
+        // vise ljudi moze da se pretplati na temu - one to many model ili ne?
+        private readonly Dictionary<int, List<Action<string>>> _subscriptions = new();
+        private readonly object _lock = new(); // Za sinhronizaciju
+
+       //   topicId -  na koju temu se korisnik pretplacuje 
+       //  Action<string> handler - funkcija koja ce se pozvati kad poruka stigne 
+        public void Subscribe(int topicId, Action<string> handler)
+        {
+            lock (_lock)
+            {
+                if (!_subscriptions.ContainsKey(topicId))
+                    _subscriptions[topicId] = new List<Action<string>>();
+
+                _subscriptions[topicId].Add(handler); //lista korisnika tj. njihova funckija koja se pozvati kad dodje poruka 
+            }
+        }
+
+        // Objavi poruku na datu temu → sve funkcije koje su pretplaćene će biti pozvane
+        public void Publish(int topicId, string message) //na koju temu se pretplacuje korisnik
+        {
+            List<Action<string>> subscribers;
+
+            lock (_lock)
+            {
+                if (!_subscriptions.TryGetValue(topicId, out subscribers))
+                    return;
+                //subscribers = new List<Action<string>>(subscribers); 
+            }
+
+            Console.WriteLine($"[PubSubEngine] Šaljem poruku za temu {topicId}: \"{message}\" ({subscribers.Count} pretplatnika)");
+
+            foreach (var handler in subscribers)
+            {
+                // Pokreni handler u zasebnoj niti (kao da šaljemo notifikaciju korisnicima)
+                new Thread(() => handler(message)).Start();
+            }
+        }
+    }
+}
